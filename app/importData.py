88ba5782d4599a78af79a8bd176256e742904e42
Gary Rogers <gari.rogers@gmail.com>
2019-03-23 11:35:07 -0500

    Implemented secrets file
    Moved database connection info to a secrets file and added a utility module to load (and cache) that file.
    
    Started adding individual functions for importing each file.

--- app/importData.py
+++ app/importData.py
@@ -1,45 +1,54 @@
 #! /usr/bin/env python3
 
 import pymssql
+import nhlstats.utils
 
-conn = pymssql.connect('localhost', 'sa', '<YourStrong!Passw0rd>', 'NHLStats')
 
-cursor = conn.cursor()
+def importGoalieStats():
+    secrets = nhlstats.utils.getSecrets()
 
-with open("../RawData/game_goalie_stats.csv", "r") as ins:
-    #array = []
-    x = 0
-    for line in ins:
-        result = [x.strip('"') for x in line.split(',')]
-        insert = "INSERT INTO game.goalie_stats ( game_id,player_id,team_id,timeOnIce,assists,goals,pim,shots,saves,powerPlaySaves,shortHandedSaves,evenSaves,shortHandedShots,evenShotsAgainst,powerPlayShotsAgainst,decisions,savePercentage,powerPlaySavePercentage,evenStrengthSavePercentage ) VALUES ({0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},'{15}',{16},{17},{18})".format(
-            result[0],
-            result[1],
-            result[2],
-            result[3],
-            result[4],
-            result[5],
-            result[6],
-            result[7],
-            result[8],
-            result[9],
-            result[10],
-            result[11],
-            result[12],
-            result[13],
-            result[14],
-            result[15],
-            result[16].replace('NA', 'NULL'),
-            result[17].replace('NA', 'NULL'),
-            result[18].strip('\n').replace('NA', 'NULL')
-        )
+    conn = pymssql.connect(
+        secrets['databases']['localSql']['server'], 
+        secrets['databases']['localSql']['username'], 
+        secrets['databases']['localSql']['password'], 
+        secrets['databases']['localSql']['database']
+    )
 
-        try:
-            if result[0].isnumeric():
-                cursor.execute(insert)
-                print('Inserted line {0}'.format(x))
-                x+=1
-        except Exception as ex:
-            print('This SQL call caused an error:')
-            print(insert)
-            raise ex
-    conn.commit()
+    cursor = conn.cursor()
+
+    with open("../RawData/game_goalie_stats.csv", "r") as ins:
+        x = 0
+        for line in ins:
+            result = [x.strip('"') for x in line.split(',')]
+            insert = "INSERT INTO game.goalie_stats ( game_id,player_id,team_id,timeOnIce,assists,goals,pim,shots,saves,powerPlaySaves,shortHandedSaves,evenSaves,shortHandedShots,evenShotsAgainst,powerPlayShotsAgainst,decisions,savePercentage,powerPlaySavePercentage,evenStrengthSavePercentage ) VALUES ({0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},'{15}',{16},{17},{18})".format(
+                result[0],
+                result[1],
+                result[2],
+                result[3],
+                result[4],
+                result[5],
+                result[6],
+                result[7],
+                result[8],
+                result[9],
+                result[10],
+                result[11],
+                result[12],
+                result[13],
+                result[14],
+                result[15],
+                result[16].replace('NA', 'NULL'),
+                result[17].replace('NA', 'NULL'),
+                result[18].strip('\n').replace('NA', 'NULL')
+            )
+
+            try:
+                if result[0].isnumeric():
+                    cursor.execute(insert)
+                    print('Inserted line {0}'.format(x))
+                    x+=1
+            except Exception as ex:
+                print('This SQL call caused an error on file line {0}:'.format(x))
+                print(insert)
+                raise ex
+        conn.commit()
---
+++ app/nhlstats/utils.py
@@ -0,0 +1,17 @@
+import json
+import os.path
+
+secrets = None
+
+def getSecrets(file = None):
+    global secrets
+
+    if secrets == None:
+        if file == None:
+            file = "/Users/grogers/Documents/Source/NHLStats/etc/secrets.json"
+
+        if os.path.isfile(file):
+            with open(file) as json_secrets:
+                secrets = json.loads(json_secrets)
+
+    return secrets
